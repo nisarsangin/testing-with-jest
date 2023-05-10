d413386edc380f3c3c80689ee5a3213ec892e4c0
Nisar Sangin <nisar.sangin@gmail.com>
2023-05-10 23:40:58 +0200

    Wrote unit and selenium tests that should fail

---
+++ tests/stack.test.js
@@ -0,0 +1,28 @@
+const stack = require('../src/stack');
+
+test('peek on empty stack returns undefined', () => {
+    expect(stack.peek()).toBeUndefined();
+});
+
+
+test('peek on stack with one element returns that element', () => {
+    stack.push(1);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(1);
+});
+
+test('peek on stack with two or more elements returns the top element', () => {
+    stack.push(1);
+    stack.push("wow");
+    stack.push(42);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(42);
+});
+
+test("pop on stack with two elements and return the top element", () => {
+    stack.push("hey");
+    stack.push("goodbye");
+
+    const element = stack.pop();
+    expect(element).toBe("hey")
+})
