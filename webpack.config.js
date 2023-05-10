ea04076efa46b655280759156124af6f21c75c49
Nisar Sangin <nisar.sangin@gmail.com>
2023-05-10 15:07:46 +0200

    Ändrade bundle.js till main.js i webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'main.js'
     },
     mode: 'production'
 };
