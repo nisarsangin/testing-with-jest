ece753eb35d29e5bffadf7a1a53cf38eb0b0cba7
Nisar Sangin <nisar.sangin@gmail.com>
2023-05-10 13:47:12 +0200

    Lade till kod i index.js och stack.js

--- src/stack.js
+++ src/stack.js
@@ -1,25 +1,18 @@
-import * as stack from './stack.js';
+var _ = require('underscore');
 
-window.onload = function () {
-    console.log("done");
+let stack = [];
 
-    var pop = document.getElementById('pop');
-    var push = document.getElementById('push');
-    var peek = document.getElementById('peek');
-    var display = document.getElementById('top_of_stack');
-
-    pop.addEventListener("click", function() {
-        var text = "Tog bort " + stack.pop();
-        alert(text);
-    });
-
-    push.addEventListener("click", function() {
-        var x = prompt("Vad ska vi lägga på stacken?");
-        stack.push(x);
-        display.innerHTML = x;
-    });
-
-    peek.addEventListener("click", function() {
-        display.innerHTML = stack.peek();
-    });
+// Lägger ett element överst i stacken
+exports.push = function (x) {
+    stack.push(x);
 };
+
+// Returnerar det översta elementet i stacken och tar bort det
+exports.pop = function () {
+    return stack.pop();
+}
+
+// Returnerar det översta elementet i stacken
+exports.peek = function () {
+    return stack[0]; // Det här är medvetet felaktigt
+}
